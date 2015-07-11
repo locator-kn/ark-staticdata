3634764d861e3fb0d50e2581647129daec3fac9c
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-07-11 22:02:42 +0200

    implement logging

---
+++ src/util/logging.ts
@@ -0,0 +1,24 @@
+var server;
+
+export function initLogging(hapiServer:any) {
+    server = hapiServer;
+}
+
+
+export function log(logging:string) {
+    if (!server) {
+        console.error('Server not initialized for logging');
+        return;
+    }
+
+    server.log(['locationpool'], logging);
+}
+
+export function logError(logging:string) {
+    if (!server) {
+        console.error('Server not initialized for logging');
+        return;
+    }
+
+    server.log(['locationpool', 'Error'], logging);
+}
